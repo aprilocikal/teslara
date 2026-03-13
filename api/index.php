c826b93bb22fba4fb326c3eb28ebad496cd291a5
aprilocikal <cikalmunazat@gmail.com>
2026-03-13 18:02:32 +0700

    fix: explicit bootstrap in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
 
-// Forward the request to the Laravel index file
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Set storage path to /tmp for Vercel's read-only environment
+$app->useStoragePath('/tmp/storage');
+
+// Handle the request
+$request = Illuminate\Http\Request::capture();
+$app->handleRequest($request);
